91bf01113aadfea03497103c71fbe018639a5eca
masato <ponsima1140@gmail.com>
2020-02-16 18:06:35 +0900

    [New] 追加

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.template')
+
+@section('content')
+    <main class="backgroundImage">
+        <div class="hero">
+            <p>Building Awasome Website</p>
+            <h1>COMING SOON</h1>
+
+            <hr>
+        </div>
+    </main>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/comingSoon.css') }}">
+@endsection
