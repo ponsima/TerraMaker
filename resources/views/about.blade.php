0dace9aa10f3facd674f292984e5d9daa3de6cca
masato <ponsima1140@gmail.com>
2020-02-26 09:50:47 +0900

    [Fix] 文字修正

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <main class="backgroundImage">
         <div class="hero">
-            <p>Building Awasome Website</p>
+            <p>Building Awesome Website</p>
             <h1>COMING SOON</h1>
 
             <hr>
