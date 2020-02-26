a628d4d880742453e8eedc7c804868cdb74a8642
masato <ponsima1140@gmail.com>
2020-02-26 09:47:17 +0900

    [Fix]

---
+++ public/js/test.js
@@ -0,0 +1,22 @@
+// var lookforward = new LookForward('.js-lookforward');
+// lookforward.on('open', function (e) {
+//     console.log('open');
+// });
+// lookforward.on('close', function (e) {
+//     console.log('close');
+// });
+// lookforward.on('closeAll', function (e) {
+//     console.log('closeAll');
+// });
+
+// new LookForward('.js-lookforward');
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    var lookforward = new LookForward('.js-lookforward');
+    // lookforward.on('open', function (e) {
+    //     console.log('open');
+    // });
+});
+
+
