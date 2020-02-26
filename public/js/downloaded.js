4308f20c825e5a7f7f338f0eb0f903afedcb9ac6
masato <ponsima1140@gmail.com>
2020-02-26 09:46:16 +0900

    [Fix]

--- public/js/downloaded.js
+++ public/js/downloaded.js
@@ -1,7 +1,5 @@
 $(function () {
-    // DLボタンクリックで発火
     $('.download-method').on('click', function () {
-        // ページ遷移
         window.location.href = '/create/preset/setting/download/finish';
     })
 });
